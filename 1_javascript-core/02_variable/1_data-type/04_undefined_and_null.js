d3366e02e9557a18e29027bc9ed7ed0c781d0318
qkrdbfl <bjsjjsjsb@naver.com>
2023-08-21 16:14:14 +0900

    https://github.com/qkrdbfl/JavaScript.git

--- 1_javascript-core/02_variable/1_data-type/04_undefined_and_null.js
+++ 1_javascript-core/02_variable/1_data-type/04_undefined_and_null.js
@@ -5,7 +5,7 @@ var undef; // 선언만 해도 undefined가 출력됨
 console.log(undef);
 
 //null 타입
-//null은 변수에 값이 없다는것을 의도적으로 명시 할때 사용한다
+//null은 변수에 값이 없다는것을 "의도적으로 명시" 할때 사용한다
 var nullVal = 'something'; // 값이 있었다가 ~ 밑에처럼 적으면
 nullVal = null; // 이전 참조를 제거하여 더 이상 'something'을 참조하지 않는다.
 console.log(nullVal);
